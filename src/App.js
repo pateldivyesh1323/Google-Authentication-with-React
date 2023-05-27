eda96693163c5354b83050baf67b1e72981a7fb0
Patel Divyesh <pateldivyesh1323@gmail.com>
2023-05-28 00:01:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import jwtDecode from 'jwt-decode';
 function App() {
+  const [user, setUser] = useState({});
+
+  const handleCallBack = (response) => {
+    const user1 = jwtDecode(response.credential);
+    setUser(user1);
+    document.getElementById('signin').hidden = true;
+  }
+
+  const handleSignOut = (e) => {
+    setUser({});
+    document.getElementById('signin').hidden = false;
+  }
+
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id: "389241942006-3auh0rqqvbqu1914v5mg3833oq4facvh.apps.googleusercontent.com",
+      callback: handleCallBack
+    })
+    google.accounts.id.renderButton(
+      document.getElementById('signin'),
+      { theme: "outline", size: "large" }
+    )
+    google.accounts.id.prompt();
+  }, [user])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+      <div id="signin" />
+      {user &&
+        <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"}}><img src={user.picture} alt="" />
+          <h3 style={{textAlign:"start"}}>{user.name}</h3></div>}
+      {Object.keys(user).length !== 0 &&
+        <button style={{marginTop:"10px",height:"30px" ,width:"60px"}}onClick={(e) => {handleSignOut(e)}}>SignOut</button>
+      }
     </div>
   );
 }
